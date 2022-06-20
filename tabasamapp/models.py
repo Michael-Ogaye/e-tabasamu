2a2d8d93f23e3ea258a3b2edd9457624fa6ff286
Michael Ogaye <hogaeymic@gmail.com>
2022-06-20 23:08:46 +0300

    creation of user account model

--- tabasamapp/models.py
+++ tabasamapp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
+class UserAccount(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='account')
+    name = models.CharField(max_length=80, blank=True)
+    account_ref= models.TextField(max_length=254, blank=True)
+    account_picture = CloudinaryField('images')
+
+    
+    
+
+    def __str__(self):
+        return f'{self.user.username} account'
+
+    @receiver(post_save, sender=User)
+    def create_user_account(sender, instance, created, **kwargs):
+        if created:
+            UserAccount.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_account(sender, instance, **kwargs):
+        instance.account.save()
