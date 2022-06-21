e384a1653779aa399318dffccca6d40c106a45da
Michael Ogaye <hogaeymic@gmail.com>
2022-06-21 18:30:24 +0300

    creation of transaction form and a facility form

---
+++ tabasamapp/forms.py
@@ -0,0 +1,33 @@
+
+from dataclasses import fields
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from . models import UserAccount,Transaction,AccountStatement,Facility
+
+
+
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+
+
+class FacilityForm(forms.ModelForm):
+    class Meta:
+        model=Facility
+        fields='__all__'
+
+
+class TransactionForm(forms.ModelForm):
+    class Meta:
+        model=Transaction
+        fields=['type','amount']
+
+
+class UpdateAccountForm(forms.ModelForm)
+
+
